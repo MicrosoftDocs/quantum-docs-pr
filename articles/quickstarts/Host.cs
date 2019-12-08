a67b9c688a789a678c4a3507b6eef35e9e658d96
geduardo <48300381+geduardo@users.noreply.github.com>
2019-12-08 21:22:09 +0100

    Add files via upload

---
+++ articles/quickstarts/Host.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using Microsoft.Quantum.Convert;
+
+namespace Quantum{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var sim = new QuantumSimulator()){
+                // First we initialize all the variables:
+                string bit_string = "0"; // To save the bit string
+                string a = "0"; // Auxiliary string variable
+                int Max = 50; // The maximum of the range 
+                bool b = false; // Auxiliary bool
+                int size = Convert.ToInt32(Math.Floor(Math.Log(Max, 2.0) + 1));
+                int output = Max + 1; // Int to store the output
+                // To calculate the amount of needed bits
+                while(output > Max){ // Loop to generate the number
+                    bit_string = "0"; // Restart the bit string if fails 
+                    for(int counter = 0; counter < size ; counter++){
+                        b = QuantumRandomNumberGenerator.Run(sim).Result; 
+                        // Call the Q# operation to give a boolean
+                        a = b ? "0" : "1"; // Transform the bool to string
+                        bit_string = bit_string + a; // Concatenate bits
+                    }
+                    output = Convert.ToInt32(bit_string, 2);
+                    // Convert the bit string to an integer
+                }
+                Console.WriteLine("The random number generated is " 
+                + output.ToString());
+                // Print the result
+            }
+        }
+    }
+    }

250a6e56ca03d8ffb22b75843fc351a5b6f8b37e
geduardo <48300381+geduardo@users.noreply.github.com>
2019-12-08 21:22:47 +0100

    Add files via upload

---
+++ articles/quickstarts/host.py
@@ -0,0 +1,19 @@
+import qsharp # Importing Q#
+from Quantum import QuantumRandomNumberGenerator # We import the 
+# quantum operation from the namespace defined in the .qs file
+max = 50 # Here we set the maximum of our range
+output = max + 1 # Variable to store the output
+while output > max:
+    bit_string = [] # We initialise a list to store the bits that
+    # will define our random integer
+    for i in range(0, max.bit_length()): # We need to call the quantum
+        # operation as many times as bits are needed to define the
+        # maximum of our range. For example, if max=7 we need 3 bits
+        # to generate all the numbers from 0 to 7. 
+        bit_string.append(QuantumRandomNumberGenerator.simulate()) 
+        # Here we call the quantum operation and store the random bit
+        # in the list
+    output = int("".join(str(x) for x in bit_string), 2) 
+# Transform bit string to integer
+
+print("The random number generated is " + str(output)) # We print the random number
